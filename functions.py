833171a2fe403823b6e6ce725bede1af9e4f7a07
VanshKhera <vanshkhera2312@gmail.com>
2022-06-16 12:38:59 +0530

    Registering a new account

---
+++ functions.py
@@ -0,0 +1,36 @@
+import sqlite3
+import os
+# Connecting Database with Python
+connection = sqlite3.connect("database.db")
+c = connection.cursor()
+
+# Creating Tables
+# Sqlite3 datatypes: null, integer, real (float), text, blob (.png, .jpg, .mp3, .mp4, etc.)
+'''
+c.execute("""
+            CREATE TABLE Customer(
+                first_name text,  
+                last_name text,
+                email text,
+                account_no integer, 
+                pin integer
+            )   
+            """)
+'''
+def newAccount():
+    os.system('cls')
+    print("\nNew Account\t\n")
+    print("Please enter the following information:\n")
+    first_name = input("First Name: ")
+    last_name = input("Last Name: ")
+    email = input("Email: ")
+    account_number = input("Account Number: ")
+    account_pin = input("Account Pin: ")
+
+    details = (first_name, last_name, email, account_number,account_pin)
+
+    c.execute("INSERT INTO Customer VALUES (?, ?, ?, ?, ?)", details)
+    connection.commit()
+    connection.close()
+    print("\nAccount created successfully!\n")
+
---
+++ main.py
@@ -0,0 +1,28 @@
+from functions import *
+
+#TODO
+commands = [
+    '1',
+    '2'
+]
+
+print("Bank Management System")
+
+with open("help.txt", "r") as f:
+    content = f.read()
+    print(content)
+
+user_input = input("\nPlease select an option between 1-10: ")
+
+while user_input != '10':
+    if user_input in commands:
+        if user_input == "1":
+            newAccount()
+            break
+        elif user_input == "2":
+            pass
+            break
+
+    print("Invalid input!")
+    user_input = input("\nPlease select an option between 1-10: ")
+
